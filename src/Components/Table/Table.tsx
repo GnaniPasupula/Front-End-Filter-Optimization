3b68b7d6c7a145eb16322ae4ccf863455916f6fc
GnaniPasupula <gnanipasupula@gmail.com>
2023-07-27 01:07:55 +0530

    Finished Filtering of Table

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Table.css';
+import Multiselect from 'multiselect-react-dropdown';
 
 interface CSVDataRow {
   number: string;
@@ -7,13 +8,14 @@ interface CSVDataRow {
   mod4: string;
   mod5: string;
   mod6: string;
+  [key: string]: string; 
 }
 
 const Table: React.FC = () => {
   const [data, setTableData] = useState<CSVDataRow[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  //const itemsPerPage = 20; 
-  const maxItemsPerPage = 100; 
+  const [filters, setFilters] = useState<{ [key: string]: string[] }>({});
+  const maxItemsPerPage = 100;
   const pageCount = Math.ceil(data.length / maxItemsPerPage);
 
   useEffect(() => {
@@ -25,12 +27,18 @@ const Table: React.FC = () => {
         const rows: CSVDataRow[] = [];
         for (let i = 1; i < lines.length; i++) {
           const values = lines[i].split(',');
+          const number = parseInt(values[0], 10);
+          console.log(number);
+          const mod3 = (number % 3).toString();
+          const mod4 = (number % 4).toString();
+          const mod5 = (number % 5).toString();
+          const mod6 = (number % 6).toString();
           const row: CSVDataRow = {
             number: values[0],
-            mod3: values[1],
-            mod4: values[2],
-            mod5: values[3],
-            mod6: values[4],
+            mod3,
+            mod4,
+            mod5,
+            mod6,
           };
           rows.push(row);
         }
@@ -69,8 +77,71 @@ const Table: React.FC = () => {
     }
   };
 
+  const handleFilterChange = (columnName: string, selectedOptions: any) => {
+    setFilters({
+      ...filters,
+      [columnName]: selectedOptions.map((option: any) => option.value),
+    });
+  };
+
+  const filterOptions = (columnName: string) => {
+    const uniqueValues = Array.from(new Set(data.map((item) => item[columnName])));
+    return uniqueValues.map((value) => ({ value, label: value }));
+  };
+
+  const filteredData = data.filter((item) => {
+    return Object.keys(filters).every((columnName) => {
+      if (filters[columnName].length === 0) return true;
+      return filters[columnName].includes(item[columnName]);
+    });
+  }).filter((item) => {
+    return data.find((d) => d.number === item.number);
+  });
+
   return (
     <div className="table-container">
+      <div className="table-filters">
+        <Multiselect
+          options={filterOptions('number')}
+          onSelect={(selectedOptions) => handleFilterChange('number', selectedOptions)}
+          onRemove={(selectedOptions) => handleFilterChange('number', selectedOptions)}
+          displayValue="label"
+          closeOnSelect={false}
+          placeholder="Select Number..."
+        />
+        <Multiselect
+          options={filterOptions('mod3')}
+          onSelect={(selectedOptions) => handleFilterChange('mod3', selectedOptions)}
+          onRemove={(selectedOptions) => handleFilterChange('mod3', selectedOptions)}
+          displayValue="label"
+          closeOnSelect={false}
+          placeholder="Select Mod3..."
+        />
+        <Multiselect
+          options={filterOptions('mod4')}
+          onSelect={(selectedOptions) => handleFilterChange('mod4', selectedOptions)}
+          onRemove={(selectedOptions) => handleFilterChange('mod4', selectedOptions)}
+          displayValue="label"
+          closeOnSelect={false}
+          placeholder="Select Mod4..."
+        />
+        <Multiselect
+          options={filterOptions('mod5')}
+          onSelect={(selectedOptions) => handleFilterChange('mod5', selectedOptions)}
+          onRemove={(selectedOptions) => handleFilterChange('mod5', selectedOptions)}
+          displayValue="label"
+          closeOnSelect={false}
+          placeholder="Select Mod5..."
+        />
+        <Multiselect
+          options={filterOptions('mod6')}
+          onSelect={(selectedOptions) => handleFilterChange('mod6', selectedOptions)}
+          onRemove={(selectedOptions) => handleFilterChange('mod6', selectedOptions)}
+          displayValue="label"
+          closeOnSelect={false}
+          placeholder="Select Mod6..."
+        />
+      </div>
       <div className="table-header sticky">
         <table>
           <thead>
@@ -88,22 +159,22 @@ const Table: React.FC = () => {
       <div className="table-body" ref={tableBodyRef}>
         <table>
           <tbody>
-            {data
+            {filteredData
               .slice((currentPage - 1) * maxItemsPerPage, currentPage * maxItemsPerPage)
               .map((item, index) => (
                 <tr key={index}>
                   <td>{(currentPage - 1) * maxItemsPerPage + index + 1}</td>
                   <td>{item.number}</td>
-                  <td>{item.mod3}</td>
-                  <td>{item.mod4}</td>
-                  <td>{item.mod5}</td>
-                  <td>{item.mod6}</td>
+                  <td>{filters['mod3']?.length && filters['mod3'][0] in item ? item[filters['mod3'][0]] : item.mod3}</td>
+                  <td>{filters['mod4']?.length && filters['mod4'][0] in item ? item[filters['mod4'][0]] : item.mod4}</td>
+                  <td>{filters['mod5']?.length && filters['mod5'][0] in item ? item[filters['mod5'][0]] : item.mod5}</td>
+                  <td>{filters['mod6']?.length && filters['mod6'][0] in item ? item[filters['mod6'][0]] : item.mod6}</td>
                 </tr>
               ))}
           </tbody>
         </table>
       </div>
-      <div className="pagination">
+      <div className="pagination sticky">
         <button onClick={() => goToPage(currentPage - 1)} disabled={currentPage === 1}>
           Previous
         </button>
